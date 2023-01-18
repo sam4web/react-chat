d3aa4cd2d099f00aa40a76b4e6b48f53f5eb2b98
Sam <sijal.m06@gmail.com>
2023-01-18 17:44:52 +0545

    [styling bug fixes] overflow-x styling issue.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,40 +6,43 @@ export default function Hero() {
   return (
     <>
       <main className='hero__section'>
-        <h1 className='title'>React Chat</h1>
-        <p className='description'>
-          A real time chat application built with{' '}
-          <a href='https://reactjs.org/' target='_blank'>
-            React
-          </a>{' '}
-          and{' '}
-          <a href='https://firebase.google.com/' target='_blank'>
-            Firebase
-          </a>
-          .{' '}
-        </p>
-        {/* content */}
+        <div className='section-container'>
+          <h1 className='title'>React Chat</h1>
+          <p className='description'>
+            A real time chat application built with{' '}
+            <a href='https://reactjs.org/' target='_blank'>
+              React
+            </a>{' '}
+            and{' '}
+            <a href='https://firebase.google.com/' target='_blank'>
+              Firebase
+            </a>
+            .{' '}
+          </p>
+          {/* content */}
 
-        <div className='btn-container'>
-          <button className='btn'>
-            <AiOutlineGoogle className='btn__icon' />
-            <span className='btn__text'>Join Chat</span>
-          </button>
+          <div className='btn-container'>
+            <button className='btn'>
+              <AiOutlineGoogle className='btn__icon' />
+              <span className='btn__text'>Join Chat</span>
+            </button>
 
-          <a
-            href='https://github.com/sam4web/react-chat'
-            className='btn btn-transparent'
-            target='_blank'
-          >
-            <span className='btn__text'>Source Code</span>
-            <FaGithub className='btn__icon' />
-          </a>
+            <a
+              href='https://github.com/sam4web/react-chat'
+              className='btn btn-transparent'
+              target='_blank'
+            >
+              <span className='btn__text'>Source Code</span>
+              <FaGithub className='btn__icon' />
+            </a>
+          </div>
+          {/* /btn-container */}
+
+          <FaReact className='background-icon bg-react' />
+          <SiFirebase className='background-icon bg-firebase' />
+          {/* /background-images */}
         </div>
-        {/* /btn-container */}
-
-        <FaReact className='background-icon bg-react' />
-        <SiFirebase className='background-icon bg-firebase' />
-        {/* /background-images */}
+        {/* /section-container */}
       </main>
     </>
   );
--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,6 @@ export default defineConfig({
   server: {
     http: 'http://localhost',
     port: 3000,
+    host: true,
   },
 });
