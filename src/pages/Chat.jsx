ce24ab1defc021389f3963843a68d49e9820886e
Sijal Manandhar <sijal.m06@gmail.com>
2023-01-28 15:54:57 +0545

    messages styling

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -9,6 +9,34 @@ export default function Chat({ user }) {
   const [message, setMessage] = useState('');
   const [showInput, setShowInput] = useState(false);
 
+  const messages = [
+    {
+      user: user,
+      body: 'Continually streamline front-end testing procedures and.',
+      posted_date: new Date().toLocaleDateString(),
+    },
+    {
+      user: user,
+      body: 'Monotonectally reinvent B2C schemas with backward-compatible catalysts.',
+      posted_date: new Date().toLocaleDateString(),
+    },
+    {
+      user: user,
+      body: 'Compellingly transform visionary methodologies for just in time methods of empowerment. Authoritatively brand multidisciplinary niches with goal-oriented.',
+      posted_date: new Date().toLocaleDateString(),
+    },
+    {
+      user: user,
+      body: 'Credibly incubate best-of-breed web services for backend potentialities. Quickly plagiarize effective portals vis-a-vis.',
+      posted_date: new Date().toLocaleDateString(),
+    },
+    {
+      user: user,
+      body: 'Credibly incubate best-of-breed web services for backend potentialities. Quickly plagiarize effective portals vis-a-vis.Credibly incubate best-of-breed web services for backend potentialities. Quickly plagiarize effective portals vis-a-vis.Credibly incubate best-of-breed web services for backend potentialities. Quickly plagiarize effective portals vis-a-vis.Credibly incubate best-of-breed web services for backend potentialities. Quickly plagiarize effective portals vis-a-vis.Credibly incubate best-of-breed web services for backend potentialities. Quickly plagiarize effective portals vis-a-vis.Credibly incubate best-of-breed web services for backend potentialities. Quickly plagiarize effective portals vis-a-vis.Credibly incubate best-of-breed web services for backend potentialities. Quickly plagiarize effective portals vis-a-vis.Credibly incubate best-of-breed web services for backend potentialities. Quickly plagiarize effective portals vis-a-vis.Credibly incubate best-of-breed web services for backend potentialities. Quickly plagiarize effective portals vis-a-vis.',
+      posted_date: new Date().toLocaleDateString(),
+    },
+  ];
+
   useEffect(() => {
     if (!user) {
       return navigate('/');
@@ -23,14 +51,31 @@ export default function Chat({ user }) {
 
   return (
     <>
-      {/* <ReactMarkdown>{message}</ReactMarkdown> */}
-
       <div className='chat'>
+        {messages.map((message) => (
+          <>
+            <div className={`message ${user === message.user ? '' : ''}`}>
+              <div className='message__image'>
+                <img src={message.user.photoURL} alt='' />
+              </div>
+              {/* /message_image */}
+              <div className='message__body'>
+                <div className='message__posted'>{message.posted_date}</div>
+                <div className='message__user'>{message.user.displayName}</div>
+                <ReactMarkdown className='message__text'>
+                  {message.body}
+                </ReactMarkdown>
+              </div>
+              {/* /message_body */}
+            </div>
+          </>
+        ))}
+
         <form className='message-form' onSubmit={(e) => handleSubmit(e)}>
           {showInput && (
             <>
               <div className='input-container'>
-                <input
+                <textarea
                   type='text'
                   className='message-form__input'
                   name='message'
@@ -68,6 +113,7 @@ export default function Chat({ user }) {
           </button>
           {/* send/show-btn */}
         </form>
+        {/* /message-form */}
       </div>
     </>
   );
